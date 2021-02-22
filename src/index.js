6c4e700418608e2d998244bf1333cef5e13539ee
lakaganth <lakaganth89@gmail.com>
2021-02-22 17:28:06 -0500

    Pagination

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { BrowserRouter as Router } from "react-router-dom";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
-import { Provider } from 'react-redux';
-import orderReducer from './store/reducers/OrdersReducers';
-import authReducer from './store/reducers/AuthReducers';
+import { Provider } from "react-redux";
+import orderReducer from "./store/reducers/OrdersReducers";
+import authReducer from "./store/reducers/AuthReducers";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-orders: orderReducer,
+  orders: orderReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  
-    <Router>
-      <Provider store={store}>
-    <App />
-      </Provider>
-    </Router>
-  ,
-  document.getElementById('root')
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
